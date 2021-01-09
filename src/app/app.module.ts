4582e0e536f43f58173d848b444bec13e1298b4c
gerhartz <paulgerhartz19@gmail.com>
2021-01-08 18:35:58 -0600

    Starter Project
    Starter Angular project with Youtube plugin added and displayed on a Home component

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { YouTubePlayerModule } from "@angular/youtube-player";
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    YouTubePlayerModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
